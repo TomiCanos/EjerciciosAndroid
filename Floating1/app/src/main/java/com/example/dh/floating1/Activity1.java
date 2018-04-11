6a2c3a1b44be140d2b29b3301eae0f71f1e1c870
Unknown <tomas.canosa@hotmail.com>
2018-04-11 18:50:46 -0300

    floating 1 stage 2

--- Floating1/app/src/main/java/com/example/dh/floating1/Activity1.java
+++ Floating1/app/src/main/java/com/example/dh/floating1/Activity1.java
@@ -1,13 +1,32 @@
 package com.example.dh.floating1;
 
+import android.content.Intent;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Activity1 extends AppCompatActivity {
+public class Activity1 extends AppCompatActivity implements FragmentActivity1.NotificadorAActivities{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_1);
+        FragmentActivity1 fragmentActivity1 = new FragmentActivity1();
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.add(R.id.container_activity1, fragmentActivity1);
+        transaction.commit();
+
+    }
+
+    @Override
+    public void recibirMensaje(String mensaje, String regalos) {
+        Intent intent = new Intent(this, Activity2.class);
+        Bundle bundle = new Bundle();
+        bundle.putString("CLAVE_MENSAJE", mensaje);
+        bundle.putString("CLAVE_REGALOS", regalos);
+        intent.putExtras(bundle);
+        startActivity(intent);
     }
 }
--- Floating1/app/src/main/java/com/example/dh/floating1/FragmentActivity1.java
+++ Floating1/app/src/main/java/com/example/dh/floating1/FragmentActivity1.java
@@ -1,11 +1,16 @@
+
 package com.example.dh.floating1;
 
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 /**
@@ -13,17 +18,35 @@ import android.view.ViewGroup;
  */
 public class FragmentActivity1 extends Fragment {
 
-
-    public FragmentActivity1() {
-        // Required empty public constructor
-    }
+    private NotificadorAActivities notificador;
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment_activity1, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        final View view = inflater.inflate(R.layout.fragment_fragment_activity1, container, false);
+
+        FloatingActionButton floatingActionButton = view.findViewById(R.id.fbbutton_fragment1_enviar_carta);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText mensaje = view.findViewById(R.id.txtedit_fragment1_mensaje);
+                EditText regalos = view.findViewById(R.id.txtedit_fragment1_regalos);
+                String regalosString = regalos.getText().toString();
+                String mensajeString = mensaje.getText().toString();
+                notificador.recibirMensaje(mensajeString, regalosString);
+            }
+        });
+        return view;
     }
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        notificador = (NotificadorAActivities) context;
+    }
+
+    public interface NotificadorAActivities {
+        public void recibirMensaje (String mensaje, String regalos);
+    }
 }
