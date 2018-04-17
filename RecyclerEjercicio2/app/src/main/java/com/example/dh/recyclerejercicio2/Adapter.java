2282a9f7bd3295256ed80a6d90b254a2ac78c333
Unknown <tomas.canosa@hotmail.com>
2018-04-16 21:55:48 -0300

    recycler 2 intentando el add

--- RecyclerEjercicio2/app/src/main/java/com/example/dh/recyclerejercicio2/Adapter.java
+++ RecyclerEjercicio2/app/src/main/java/com/example/dh/recyclerejercicio2/Adapter.java
@@ -2,10 +2,12 @@ package com.example.dh.recyclerejercicio2;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -15,23 +17,27 @@ import java.util.zip.Inflater;
 
 public class Adapter extends RecyclerView.Adapter {
     private List listaProductos;
-    private Context context;
+    private NotificableDelClickRecycler notificableDelClickRecycler;
 
-    public Adapter(List listaProductos, Context context) {
+    public Adapter(List listaProductos, NotificableDelClickRecycler notificableDelClickRecycler) {
         this.listaProductos = listaProductos;
-        this.context = context;
+        this.notificableDelClickRecycler = notificableDelClickRecycler;
+
     }
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        Inflater inflater = new Inflater();
-        
-        return null;
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.producto, parent, false);
+        ViewHolderProductos viewHolderProductos = new ViewHolderProductos(view);
+        return viewHolderProductos;
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-
+        final Producto producto = (Producto) listaProductos.get(position);
+        final ViewHolderProductos viewHolderProductos = (ViewHolderProductos) holder;
+        viewHolderProductos.bindProducto(producto);
     }
 
     @Override
@@ -39,20 +45,33 @@ public class Adapter extends RecyclerView.Adapter {
         return listaProductos.size();
     }
 
-    private class viewHolderProductos extends RecyclerView.ViewHolder {
-       private TextView nombreProducto;
-       private TextView precioProducto;
-       private ImageView imagenProducto;
-        public viewHolderProductos(View itemView) {
+    private class ViewHolderProductos extends RecyclerView.ViewHolder {
+        private TextView nombreProducto;
+        private TextView precioProducto;
+        private ImageView imagenProducto;
+
+        public ViewHolderProductos(View itemView) {
             super(itemView);
             nombreProducto = itemView.findViewById(R.id.txtview_nombre_producto);
             precioProducto = itemView.findViewById(R.id.txtview_precio_producto);
             imagenProducto = itemView.findViewById(R.id.imageview_fondo_producto);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Producto producto = (Producto) listaProductos.get(getAdapterPosition());
+                    notificableDelClickRecycler.notificarClick(producto);
+                }
+            });
         }
-        public void bindProducto(Producto producto){
+
+        public void bindProducto(Producto producto) {
             nombreProducto.setText(producto.getNombre());
             precioProducto.setText(producto.getPrecio());
             imagenProducto.setImageResource(producto.getImagen());
         }
+    }
+
+    public interface NotificableDelClickRecycler {
+        void notificarClick(Producto producto);
     }
 }
--- RecyclerEjercicio2/app/src/main/java/com/example/dh/recyclerejercicio2/FragmentListaObjetos.java
+++ RecyclerEjercicio2/app/src/main/java/com/example/dh/recyclerejercicio2/FragmentListaObjetos.java
@@ -1,26 +1,57 @@
 package com.example.dh.recyclerejercicio2;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class FragmentListaObjetos extends Fragment {
+public class FragmentListaObjetos extends Fragment implements Adapter.NotificableDelClickRecycler {
 
-
+    private List<Producto> productos;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
+        productos = new ArrayList<>();
+        for (int i = 0; i<10; i++){cargarProductos("moto cruda", "$15600", R.drawable.moto);}
         View view = inflater.inflate(R.layout.fragment_fragment_lista_objetos, container, false);
         RecyclerView recyclerView = view.findViewById(R.id.recyclerView_fragment_objetos);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
+        recyclerView.setHasFixedSize(true);
+        Adapter adapter = new Adapter(productos, this);
+        recyclerView.setAdapter(adapter);
+        FloatingActionButton floatingActionButton = view.findViewById(R.id.fbutton_agregar_producto);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cargarProductos("ropero", "$5500", R.drawable.ropero);
+
+            }
+        });
         return view;
     }
 
+    private void cargarProductos(String nombre, String precio, Integer foto) {
+        Producto producto = new Producto(nombre, precio, foto);
+        productos.add(producto);
+        }
+
+
+    @Override
+    public void notificarClick(Producto producto) {
+        Toast.makeText(getContext(), "producto clickeado: " + producto.getNombre(), Toast.LENGTH_SHORT).show();
+        ;
+    }
 }
